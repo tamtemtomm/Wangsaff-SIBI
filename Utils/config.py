e2ba9799252d33a153cf17812fda991458651d84
Timo <92860332+tamtemtomm@users.noreply.github.com>
2023-09-15 06:46:23 +0700

    Delete Utils/config.py

--- Utils/config.py
+++
@@ -1,4 +0,0 @@
-KERAS_MODEL_PATH = '.\models\SIBIModelKeras.h5'
-TORCH_MODEL_PATH = '.\models\SIBIModelTorch.pth'
-VIDEO_TEST_PATH = r".\Utils\test\test2.mp4"
-OUTPUT_VIDEO_TEST_PATH = r".\Utils\test\output_test2.mp4"
